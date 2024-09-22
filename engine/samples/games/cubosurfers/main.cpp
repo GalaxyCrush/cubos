80edd5f846cf2478fe63c34ee6d60e21b4e79de9
João Pereira <joaomrpereira0@gmail.com>
2024-09-22 15:20:41 +0100

    Falta por o player a voar e IMGUI

--- engine/samples/games/cubosurfers/main.cpp
+++ engine/samples/games/cubosurfers/main.cpp
@@ -10,11 +10,11 @@
 #include <cubos/engine/voxels/plugin.hpp>
 
 #include "armor.hpp"
+#include "jetpack.hpp"
 #include "obstacle.hpp"
 #include "player.hpp"
 #include "spawner.hpp"
 #include "total_time.hpp"
-
 using namespace cubos::engine;
 
 static const Asset<Scene> SceneAsset = AnyAsset("ee5bb451-05b7-430f-a641-a746f7009eef");
@@ -32,6 +32,7 @@ int main()
     cubos.plugin(playerPlugin);
     cubos.plugin(timePlugin);
     cubos.plugin(armorPlugin);
+    cubos.plugin(jetpackPlugin);
     cubos.plugin(obstaclePlugin);
 
     cubos.startupSystem("configure settings").tagged(settingsTag).call([](Settings& settings) {
